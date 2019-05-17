1fb0972cfde0ca08624b69667f58a73d5ae3b282
Jesuslovestheworld <1367346501@qq.com>
2019-05-17 11:47:34 +0800

    1.检测版本工具类

---
+++ app/src/main/java/com/baidu/traveleverywhere/utils/UIUtils.java
@@ -0,0 +1,45 @@
+package com.baidu.traveleverywhere.utils;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.baidu.traveleverywhere.base.BaseApp;
+
+/**
+ * Created by xts on 2017/3/9.
+ * @描述 Ui相关的工具类,含Context获取,资源获取,包名
+ */
+
+public class UIUtils {
+	/**得到上下文*/
+	public static Context getContext() {
+		return BaseApp.getInstance();
+	}
+
+	/**得到Resource对象*/
+	public static Resources getResources() {
+		return getContext().getResources();
+	}
+
+	/**得到string.xml中的字符*/
+	public static String getString(int resId) {
+		return getResources().getString(resId);
+	}
+
+	/**得到string.xml中的字符数组*/
+	public static String[] getStringArr(int resId) {
+		return getResources().getStringArray(resId);
+	}
+
+	/**得到color.xml中的颜色值*/
+	public static int getColor(int resId) {
+		return getResources().getColor(resId);
+	}
+
+	/**得到应用程序的包名*/
+	public static String getPackageName() {
+		return getContext().getPackageName();
+	}
+
+}
